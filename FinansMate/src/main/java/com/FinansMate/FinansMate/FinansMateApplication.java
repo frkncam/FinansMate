51ba0ba9b5f8174fe26f04cc0c7c398434ba9e51
frkncam <furkancam0246@gmail.com>
2025-03-25 20:46:48 +0300

    Hello
    FinansMate

---
+++ FinansMate/src/main/java/com/FinansMate/FinansMate/FinansMateApplication.java
@@ -0,0 +1,13 @@
+package com.FinansMate.FinansMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansMateApplication.class, args);
+	}
+
+}
