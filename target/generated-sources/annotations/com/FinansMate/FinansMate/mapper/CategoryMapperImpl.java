2a671728c26a3d466a0b068fb1ffd70df13add4b
Furkan ÇAM <furkancam0246@gmail.com>
2025-05-20 16:35:08 +0300

    TransactionType refactor

--- target/generated-sources/annotations/com/FinansMate/FinansMate/mapper/CategoryMapperImpl.java
+++ target/generated-sources/annotations/com/FinansMate/FinansMate/mapper/CategoryMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-20T12:40:16+0300",
+    date = "2025-05-20T16:27:06+0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
